48b7c2c29393c7d3ded713c150c7884b0d7697ed
Chris Kimpton <chris@kimptoc.net>
2014-09-13 11:54:56 +0100

    number range tests

--- src/test/java/org/boon/datarepo/DataRepoPerfTest.java
+++ src/test/java/org/boon/datarepo/DataRepoPerfTest.java
@@ -8,6 +8,7 @@ import java.util.concurrent.Executor;
 import static org.boon.Boon.puts;
 import static org.boon.criteria.ObjectFilter.and;
 import static org.boon.criteria.ObjectFilter.eq;
+import static org.boon.criteria.ObjectFilter.gt;
 import static org.testng.Assert.assertTrue;
 
 public class DataRepoPerfTest
@@ -25,6 +26,7 @@ public class DataRepoPerfTest
                 .primaryKey("name")
                 .lookupIndex("sport")
                 .lookupIndex("job")
+                .lookupIndex("experience")
                 .lookupIndex("colour")
                 .build(String.class, Map.class);
 
@@ -36,6 +38,7 @@ public class DataRepoPerfTest
             entry.put("colour",colours[((int) (Math.random() * colours.length))]);
             entry.put("job",jobs[((int) (Math.random() * jobs.length))]);
             entry.put("sport",sports[((int) (Math.random() * sports.length))]);
+            entry.put("experience", i%100);
             database.add(entry);
         }
 
@@ -43,7 +46,7 @@ public class DataRepoPerfTest
 
         timeTask(dbRepo, "Brute force lookup", 700, 10000, new RunThing() {
             public Object go(Object param) {
-                List results = null;
+                List results;
                 results = new ArrayList();
                 for (Map<String, Object> entry : database) {
                     if (entry.get("colour").equals("red")) {
@@ -54,13 +57,38 @@ public class DataRepoPerfTest
             }
         });
 
-        timeTask(dbRepo, "DataRepo colour query", 30, 10000, new RunThing() {
+        timeTask(dbRepo, "Brute force number lookup", 700, 10000, new RunThing() {
+            public Object go(Object param) {
+                List results = new ArrayList();
+                for (Map<String, Object> entry : database) {
+                    if ( (Integer)entry.get("experience") > 50) {
+                        results.add(entry);
+                    }
+                }
+                return results;
+            }
+        });
+
+        timeTask(dbRepo, "Brute force number/compareTo lookup", 700, 10000, new RunThing() {
+            Integer fifty = 50;
+            public Object go(Object param) {
+                List results = new ArrayList();
+                for (Map<String, Object> entry : database) {
+                    if ( fifty.compareTo ((Integer)entry.get("experience")) > 0 ) {
+                        results.add(entry);
+                    }
+                }
+                return results;
+            }
+        });
+
+        timeTask(dbRepo, "DataRepo colour query", 60, 10000, new RunThing() {
             public Object go(Object param) {
                 return dbRepo.query(eq("colour", "red"));
             }
         });
 
-        timeTask(dbRepo, "DataRepo job query", 30, 10000, new RunThing() {
+        timeTask(dbRepo, "DataRepo job query", 60, 10000, new RunThing() {
             public Object go(Object param) {
                 return dbRepo.query(eq("job", "artist"));
             }
@@ -75,6 +103,25 @@ public class DataRepoPerfTest
         timeTask(dbRepo, "DataRepo filtered lookup", 650, 1000, new RunThing() {
             public Object go(Object param) {
                 return dbRepo.results(eq("sport", "swim")).filter(eq("colour", "red")); // TODO SLOW - dont use
+            }
+        });
+
+        timeTask(dbRepo, "DataRepo lookup and manual", 500, 1000, new RunThing() {
+            public Object go(Object param) {
+                Collection<Map<String, Object>> results2 = new ArrayList<Map<String, Object>>(); // TODO SLOW - dont use
+                ResultSet<Map<String, Object>> results = dbRepo.results(eq("sport", "swim")); // TODO SLOW - dont use
+                for (Map<String, Object> result : results) {
+                    if (result.get("colour").equals("red")) {
+                        results2.add(result);
+                    }
+                }
+                return results2;
+            }
+        });
+
+        timeTask(dbRepo, "DataRepo number range", 5000, 1000, new RunThing() {
+            public Object go(Object param) {
+                return dbRepo.query(gt("experience", 50));
             }
         });
 
